63cac0f0b88f2da3247632cf78fa313e42cdb75b
kaung721 <167502418+kaung721@users.noreply.github.com>
2025-11-22 21:34:19 -0500

    added he package, title bug fixed

--- backend/services/newsService.js
+++ backend/services/newsService.js
@@ -1,17 +1,19 @@
 const RSSParser = require('rss-parser');
+const he = require('he');
 const parser = new RSSParser();
 
 async function getFromRSS(url, category = 'general') {
   const feed = await parser.parseURL(url);
   return (feed.items || []).map(i => {
 
+    let decodedTitle = he.decode(i.title || '');
     let description = i.contentSnippet || i.summary || '';
     if(description.length > 100) {
       description = description.slice(0, 100) + '...';
     }
 
     return {
-    title: i.title,
+    title: decodedTitle,
     description,
     url: i.link,
     source: feed.title,
